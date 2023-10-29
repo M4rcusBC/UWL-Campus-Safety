b1fe6668a817e22a2e54dc83c0e951fd60c70aa4
Marcus C <97267316+M4rcusBC@users.noreply.github.com>
2023-10-29 17:28:23 -0500

    fixing stupid bugs i caused myself

--- app/src/main/java/com/example/eagleguard/ui/map/MapFragment.kt
+++ app/src/main/java/com/example/eagleguard/ui/map/MapFragment.kt
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.example.eagleguard.databinding.FragmentNotificationsBinding
+import com.example.eagleguard.databinding.FragmentMapBinding
 
 class MapFragment : Fragment() {
 
-    private var _binding: FragmentNotificationsBinding? = null
+    private var _binding: FragmentMapBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -25,10 +25,10 @@ class MapFragment : Fragment() {
         val notificationsViewModel =
             ViewModelProvider(this)[MapViewModel::class.java]
 
-        _binding = FragmentNotificationsBinding.inflate(inflater, container, false)
+        _binding = FragmentMapBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textNotifications
+        val textView: TextView = binding.textView
         notificationsViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
